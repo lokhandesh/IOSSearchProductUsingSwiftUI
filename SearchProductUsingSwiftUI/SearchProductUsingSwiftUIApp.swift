2a0c546245a2b88aa8d98af70398fc1cd32d361c
santoshlokhande <lokhandesh@gmail.com>
2024-08-01 17:49:56 +0800

    Initial Commit

---
+++ SearchProductUsingSwiftUI/SearchProductUsingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchProductUsingSwiftUIApp.swift
+//  SearchProductUsingSwiftUI
+//
+//  Created by santosh lokhande on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchProductUsingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
